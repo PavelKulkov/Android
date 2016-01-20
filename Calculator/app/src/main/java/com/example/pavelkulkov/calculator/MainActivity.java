86422f866b59453931048c8a7e9fee4d438b56ec
PavelKulkov <pawel.kulkoff@yandex.ru>
2016-01-20 07:32:30 +0300

    Added

---
+++ Calculator/app/src/main/java/com/example/pavelkulkov/calculator/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.pavelkulkov.calculator;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    public final static String KEY = "com.example.pavelkulkov.calculator";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void addition(View view){
+
+        Intent intent = new Intent (this,AnswerActivity.class);
+
+        EditText firstNumber = (EditText)findViewById(R.id.firstNumber);
+        EditText secondNumber = (EditText)findViewById(R.id.secondNumber);
+        int tmp = 0;
+        String answer="";
+        try{
+             tmp = Integer.parseInt(firstNumber.getText().toString())
+                    + Integer.parseInt(secondNumber.getText().toString());
+            answer = firstNumber.getText().toString() + "+" + secondNumber.getText().toString()+"="+tmp;
+
+        }catch (Exception ex){
+            answer = "Неверные данные";
+        }
+        intent.putExtra(KEY, answer);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(intent);
+    }
+}
